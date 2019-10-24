2d71886c8feeeb291988bfa476a294307f39a823
Gummibeer <dev.gummibeer@gmail.com>
2019-10-24 10:22:56 +0200

    style adjustments and refactor to tailwind classes

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -1,65 +1,55 @@
 @extends('master')
 
 @section('content')
-    <header>
-        <div>
+    <header class="relative">
+        <div class="relative z-10">
             <nav>
-                <div class="container mx-auto px-4">
-                    <h1>
-                        <i class="fad fa-user-astronaut fa-fw"></i>
-                        <span>Astrotomic</span>
+                <div class="container mx-auto px-4 flex items-center">
+                    <h1 class="font-semibold text-white text-2xl leading-none tracking-wider py-4 flex-grow flex items-center">
+                        <i class="fad fa-user-astronaut fa-fw text-4xl mr-2"></i>
+                        <span class="py-2">Astrotomic</span>
                     </h1>
 
-                    <ul>
-                        <li>
-                            <a href="https://github.com/Astrotomic" class="btn">
-                                <i class="fab fa-github"></i>
-                                GitHub
-                            </a>
-                        </li>
-                    </ul>
+                    <a href="https://github.com/Astrotomic" class="btn">
+                        <i class="fab fa-github"></i>
+                        GitHub
+                    </a>
                 </div>
             </nav>
 
-            <div class="lead container mx-auto px-4">
-                <strong>
+            <div class="container mx-auto px-4 py-64 text-center">
+                <strong class="text-6xl text-white font-bold">
                     <span>Open Source</span>
                     <span>PHP</span>
                     <span>Laravel</span>
                 </strong>
-                <p>
+                <p class="text-2xl mb-8">
                     We want to provide helpful, solid and easy to use open source packages.
                     <br/>
                     Most of them will be for Laravel - but sometimes also plain PHP.
                 </p>
 
                 <div class="flex flex-row flex-wrap justify-center">
-                    <div class="px-8 pb-16">
-                        <div class="border-2 border-gray-400 p-4 rounded-lg">
-                            <div class="px-8">
-                                <i class="fad fa-box-heart fa-fw fa-5x"></i>
-                            </div>
-                            <strong class="block py-4 text-3xl text-white">123456</strong>
-                            <span class="block border-t-2 border-gray-400 pt-2">packages</span>
+                    <div class="lg:w-1/4 px-8 pt-16">
+                        <div class="mb-4">
+                            <i class="fad fa-box-heart fa-fw fa-5x"></i>
                         </div>
+                        <strong class="block text-4xl text-white">123456</strong>
+                        <span class="block text-white opacity-50 text-xl">packages</span>
                     </div>
-                    <div class="px-8 pb-16">
-                        <div class="border-2 border-gray-400 p-4 rounded-lg">
-                            <div class="px-8">
-                                <i class="fad fa-users-cog fa-fw fa-5x"></i>
-                            </div>
-                            <strong class="block py-4 text-3xl text-white">123456</strong>
-                            <span class="block border-t-2 border-gray-400 pt-2">contributors</span>
+                    <div class="lg:w-1/4 px-8 pt-16">
+                        <div class="mb-4">
+                            <i class="fad fa-users-cog fa-fw fa-5x"></i>
                         </div>
+                        <strong class="block text-4xl text-white">123456</strong>
+                        <span class="block text-white opacity-50 text-xl">packages</span>
                     </div>
-                    <div class="px-8">
-                        <div class="border-2 border-gray-400 p-4 rounded-lg">
-                            <div class="px-8">
-                                <i class="fad fa-code-commit fa-fw fa-5x"></i>
-                            </div>
-                            <strong class="block py-4 text-3xl text-white">123456</strong>
-                            <span class="block border-t-2 border-gray-400 pt-2">commits</span>
+                    <div class="lg:w-1/4 px-8 pt-16">
+                        <div class="mb-4">
+                            <i class="fad fa-code-commit fa-fw fa-5x"></i>
                         </div>
+                        <strong class="block text-4xl text-white">123456</strong>
+                        <span class="block text-white opacity-50 text-xl">packages</span>
                     </div>
                 </div>
             </div>
@@ -94,7 +84,7 @@
             <div class="w-full lg:w-1/2 p-8 flex flex-col">
                 <h2 class="text-white text-3xl mb-3">Laravel Translatable</h2>
                 <p class="flex-grow">This is a Laravel package for translatable models. Its goal is to remove the complexity in retrieving and storing multilingual model instances. With this package you write less code, as the translations are being fetched/saved when you fetch/save your instance.</p>
-                <a href="https://github.com/Astrotomic/stancy" class="btn mt-4 self-start">
+                <a href="https://github.com/Astrotomic/laravel-translatable" class="btn mt-4 self-start">
                     <i class="fab fa-github fa-fw"></i>
                     astrotomic/laravel-translatable
                 </a>
