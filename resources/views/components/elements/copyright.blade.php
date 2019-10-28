c672e66942749586f744378f097ae1c29abe028d
Gummibeer <dev.gummibeer@gmail.com>
2019-10-28 15:52:26 +0100

    add gummibeer author

--- resources/views/components/elements/copyright.blade.php
+++ resources/views/components/elements/copyright.blade.php
@@ -1,10 +1,16 @@
 <footer>
     <div class="container mx-auto p-4 flex items-center flex-wrap">
-        <span class="flex-grow whitespace-no-wrap opacity-50">
+        <span class="flex-grow">
+            <span class="whitespace-no-wrap opacity-50">
             Astrotomic &copy; 2019
             @if(date('Y') > 2019)
                 - {{ date('Y') }}
             @endif
+            </span>
+            <span class="whitespace-no-wrap">
+                <span class="opacity-50">created by</span>
+                <a-styled href="https://gummibeer.de">Gummibeer</a-styled>
+            </span>
         </span>
 
         @foreach($links as $link)
@@ -18,7 +24,7 @@
         <span class="opacity-50">Powered by</span>
         <span class="divided">
             <a-styled href="https://github.com/Astrotomic/stancy">Stancy</a-styled>
-            <a-styled href="https://www.netlify.com">Netlify</a-styled>
+            <a-styled href="https://netlify.com">Netlify</a-styled>
             <a-styled href="https://tailwindcss.com">tailwindcss</a-styled>
             <a-styled href="https://fontawesome.com">Font Awesome</a-styled>
             <a-styled href="https://rsms.me/inter">Inter</a-styled>
