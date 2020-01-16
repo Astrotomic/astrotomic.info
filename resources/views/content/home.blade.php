bcbf33d4fe746a14a3acd9e0187ec0a3c214024f
Tom Witkowski <dev.gummibeer@gmail.com>
2020-01-16 11:22:38 +0100

    add license/treeware section

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -53,7 +53,39 @@
                 <contributor-badge :contributor="$contributor" />
             @endforeach
         </div>
-        <wave class="fill-current text-astrotomic-400 bg-background mb-8" />
+    </section>
+
+    <section class="bg-treeware-500 relative">
+        <wave class="fill-current text-astrotomic-400 bg-treeware-500 mb-16" />
+        <h2 class="container mx-auto text-white mb-4 text-4xl px-4">License: MIT & Treeware</h2>
+        <div class="container mx-auto pr-4 mb-8 flex flex-wrap">
+            <img src="https://img.shields.io/badge/License-MIT-blue?style=for-the-badge" class="ml-4" />
+            <img src="https://img.shields.io/badge/Treeware-%F0%9F%8C%B3-lightgreen?style=for-the-badge" class="ml-4" />
+        </div>
+        <div class="container mx-auto px-4 flex flex-col md:flex-row">
+            <div class="md:w-2/3 md:pr-4">
+                <p class="mb-2">
+                    If not other stated all our packages are licensed under MIT License (copy of license is in each package) and also as Treeware.
+                </p>
+                <p class="mb-2">
+                    You're free to use our packages, but if one makes it to your production environment you are required to buy the world a trees (at least the lowest package <a href="https://offset.earth/treeware" class="inline-block opacity-50 hover:opacity-100 border-b border-dotted border-white">offset.earth/treeware</a> offers).
+                </p>
+                <p class="mb-2">
+                    It’s now common knowledge that one of the best tools to tackle the climate crisis and keep our temperatures from rising above 1.5C is to plant trees. If you support this package and contribute to the Treeware forest you’ll be creating employment for local families and restoring wildlife habitats.
+                </p>
+                <p class="mb-2">
+                    You can buy trees at <a href="https://offset.earth/treeware" class="inline-block opacity-50 hover:opacity-100 border-b border-dotted border-white">offset.earth/treeware</a>.
+                    <br>
+                    Read more about Treeware at <a href="https://treeware.earth" class="inline-block opacity-50 hover:opacity-100 border-b border-dotted border-white">treeware.earth</a>.
+                </p>
+            </div>
+            <div class="md:w-1/3 mt-4 md:mt-0">
+                <a href="https://offset.earth/treeware">
+                    <img src="https://toolkit.offset.earth/carbonpositiveworkforce/badge/5e186e68516eb60018c5172b?white=true&landscape=true" />
+                </a>
+            </div>
+        </div>
+        <wave class="fill-current text-treeware-500 bg-background mb-8" />
     </section>
 
     <copyright/>
