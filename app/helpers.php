0942d0f8611f13c3440f112962f8658fe8d22822
Tom Witkowski <dev.gummibeer@gmail.com>
2020-02-27 13:36:59 +0100

    prevent upscale and use device pixel ratio

--- app/helpers.php
+++ app/helpers.php
@@ -9,6 +9,6 @@ if (! function_exists('image')) {
             return $url;
         }
 
-        return 'https://images.weserv.nl?il&output='.$format.($width ? '&w='.$width : null).'&url='.urlencode(url($url));
+        return 'https://images.weserv.nl?il&we&dpr=2&output='.$format.($width ? '&w='.$width : null).'&url='.urlencode(url($url));
     }
 }
--- resources/views/components/contributorBadge.blade.php
+++ resources/views/components/contributorBadge.blade.php
@@ -10,7 +10,7 @@
         @endif
     "
 >
-    <img src="https://images.weserv.nl?il&w=96&output=jpg&url={{ urlencode($contributor['avatar_url']) }}" alt="{{ $contributor['login'] }} Avatar" class="w-12 h-12" />
+    <img src="https://images.weserv.nl?il&we&dpr=2&w=48&output=jpg&url={{ urlencode($contributor['avatar_url']) }}" alt="{{ $contributor['login'] }} Avatar" class="w-12 h-12" />
     <span class="pl-4 pr-2 flex flex-grow">{{ $contributor['login'] }}</span>
     <span class="opacity-50 pr-4">{{ $contributor['commits'] }}</span>
 </a>
