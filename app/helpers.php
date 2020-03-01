e228cd7eeda0842471911843fb55c2920a558c67
Tom Witkowski <dev.gummibeer@gmail.com>
2020-03-01 10:28:18 +0100

    fix php cs

--- app/helpers.php
+++ app/helpers.php
@@ -10,8 +10,8 @@ if (! function_exists('picture')) {
         string $class = ''
     ): HtmlString {
         return $url->toPicture([
-            '1x' => fn(Url $url) => $url->dpr(1),
-            '2x' => fn(Url $url) => $url->dpr(2),
+            '1x' => fn (Url $url) => $url->dpr(1),
+            '2x' => fn (Url $url) => $url->dpr(2),
         ], $alt, $class);
     }
 }
