100f8210fc447b19a9b2cbb7e2ab329c9f7d7cdb
Tom Witkowski <dev.gummibeer@gmail.com>
2020-08-06 10:56:48 +0200

    enforce block for SVG and scale to 105% and cut with container https://twitter.com/PerSteinhorst/status/1291287238148587520

--- resources/views/components/elements/wave.blade.php
+++ resources/views/components/elements/wave.blade.php
@@ -1,3 +1,5 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 3594 343" class="transform scale-100 {{ $class }}">
-    <path d="M-7,311.773S857.694,141.03,1933,270.8c1113.66,134.4,1661,39.972,1661,39.972V0H-8Z"></path>
-</svg>
+<div class="overflow-hidden">
+    <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 3594 343" class="block transform scale-105 {{ $class }}">
+        <path d="M-7,311.773S857.694,141.03,1933,270.8c1113.66,134.4,1661,39.972,1661,39.972V0H-8Z"></path>
+    </svg>
+</div>
