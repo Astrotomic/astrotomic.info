d764a06299462f0b5e1e86fad9cfe37dddcd2770
Tom Witkowski <dev.gummibeer@gmail.com>
2021-03-10 16:29:43 +0100

    reduce code complexity

--- resources/views/content/contributor.blade.php
+++ resources/views/content/contributor.blade.php
@@ -19,41 +19,29 @@
             </h2>
 
             <ul class="flex flex-col justify-between my-2 mx-auto space-y-2 space-x-0 list-inline md:max-w-2xl sm:space-x-4 sm:space-y-0 sm:flex-row">
-                @if(isset($info['location']))
+                @if(!empty($info['location']))
                     <li>
                         <icon icon="fa-map-marker-alt" icon-style="fas" class="mr-1 opacity-75" />
-                        {{$info['location']}}
+                        {{ $info['location'] }}
                     </li>
                 @endif
-                @if(!$info['blog']=="")
+                @if(!empty($info['blog']))
                     @php
-                        $website = $info['blog']; 
-                        $link = $info['blog'];
-                        if(substr($website, 0, 8) == "https://"){
-                            $website = substr($website, 8);    
-                        }elseif(substr($website, 0, 7) === "http://"){
-                            $website = substr($website, 7);  
-                        }elseif(substr($website, 0, 8) != "https://" || substr($website, 0, 7) != "http://" ){
-                            $link = "https://".$website;
-                        }
+                        $website = rtrim(str_replace(['http://', 'https://'], '', $info['blog']), '/');
 
-                        if(strlen($website) > 20 && (isset($info['location']) && isset($info['twitter_username']))){
-                            $website = substr($website, 0, 20) . ' ...';
-                        }
-
-                        if(substr($website, -1) === "/"){
-                            $website = substr($website, 0, -1); 
+                        if (mb_strlen($website) > 30) {
+                            $website = parse_url($info['blog'], PHP_URL_HOST) ?? substr($website, 0, 30) . '…';
                         }
                     @endphp
 
-                    <div class="flex-1">
-                        <icon icon="fa-browser" icon-style="fas" class="opacity-75 mr-1" />
-                        <a-styled :href="$link" :underlined="true">{{$website}}</a-styled>
-                    </div>
-                @endif
-                @if(isset($info['twitter_username']))
                     <li>
-                        <icon icon="fa-twitter" icon-style="fab" class="mr-1 opacity-75" />
+                        <icon icon="fa-globe" icon-style="fas" class="mr-1 opacity-75"/>
+                        <a-styled :href="$info['blog']" :underlined="true">{{ $website }}</a-styled>
+                    </li>
+                @endif
+                @if(!empty($info['twitter_username']))
+                    <li>
+                        <icon icon="fa-twitter" icon-style="fab" class="mr-1 opacity-75"/>
                         <a-styled :href="'https://www.twitter.com/'.$info['twitter_username']" underlined>
                             {{ '@'.$info['twitter_username'] }}
                         </a-styled>
@@ -61,15 +49,15 @@
                 @endif
             </ul>
 
-            @if(isset($info['bio']))
-            <div class="py-2 mx-auto mt-2 text-2xl md:max-w-3xl">
-                <p>{{$info['bio']}}</p>
-            </div>
+            @if(!empty($info['bio']))
+                <div class="py-2 mx-auto mt-2 text-2xl md:max-w-3xl">
+                    <p>{{ $info['bio'] }}</p>
+                </div>
             @endif
 
             <div class="flex flex-row flex-wrap justify-center">
-                <count-up icon="fa-box-heart" :value="count($packages)" label="packages" />
-                <count-up icon="fa-code-commit" :value="$commits" label="commits" />
+                <count-up icon="fa-box-heart" :value="count($packages)" label="packages"/>
+                <count-up icon="fa-code-commit" :value="$commits" label="commits"/>
             </div>
 
 
