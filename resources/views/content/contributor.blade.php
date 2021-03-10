f5cadbcc91af45baf5dbdb2eb225286c52abb04d
Tom Witkowski <dev.gummibeer@gmail.com>
2021-03-10 12:09:42 +0100

    sort tailwind classes with Tailwind formatter

--- resources/views/content/contributor.blade.php
+++ resources/views/content/contributor.blade.php
@@ -9,41 +9,40 @@
 
 @section('content')
     <hero>
-        <div class="container mx-auto px-4 py-32 text-center">
-            <imgix :src="$avatar_url" width="256" height="256" ratio="1:1" :alt="$login.' Avatar'" class="mx-auto rounded w-64 h-64" />
-            <h2 class="mt-2 text-5xl text-white font-bold">
-                @if(isset($info['name']))
-                    <a-styled :href="'https://github.com/'.$login">{{$info['name']}}</a-styled>
-                @else
-                    <a-styled :href="'https://github.com/'.$login">{{$login}}</a-styled>
-                @endif
+        <div class="container px-4 pt-4 pb-16 mx-auto text-center sm:py-32">
+            <imgix :src="$avatar_url" width="256" height="256" ratio="1:1" :alt="$login.' Avatar'" class="mx-auto w-64 h-64 rounded" />
+            <h2 class="mt-2 text-4xl font-bold text-white sm:text-5xl">
+                <span>{{ $info['name'] ?? $login }}</span>
+                <a-styled :href="$html_url" class="p-1 ml-1 text-3xl sm:text-4xl">
+                    <icon icon="fa-github" icon-style="fab"/>
+                </a-styled>
             </h2>
 
-            <div class="mx-auto md:max-w-2xl">
-                <div class="py-2 space-x-4 flex justify-between">
-                    @if(isset($info['location']))
-                        <div class="flex-1">
-                            <icon icon="fa-location-circle" icon-style="fas" class="opacity-75 mr-1" />
-                            {{$info['location']}}
-                        </div>
-                    @endif
-                    @if(!$info['blog']=="")
-                        <div class="flex-1">
-                            <icon icon="fa-browser" icon-style="fas" class="opacity-75 mr-1" />
-                            <a-styled :href="$info['blog']" :underlined="true">{{$info['blog']}}</a-styled>
-                        </div>
-                    @endif
-                    @if(isset($info['twitter_username']))
-                        <div class="flex-1">
-                            <icon icon="fa-at" icon-style="fas" class="opacity-75 mr-1" />
-                            <a-styled :href="'https://www.twitter.com/'.$info['twitter_username']" :underlined="true">{{$info['twitter_username']}}</a-styled>
-                        </div>
-                    @endif
-                </div>
-            </div>
+            <ul class="flex flex-col justify-between my-2 mx-auto space-y-2 space-x-0 list-inline md:max-w-2xl sm:space-x-4 sm:space-y-0 sm:flex-row">
+                @if(isset($info['location']))
+                    <li>
+                        <icon icon="fa-map-marker-alt" icon-style="fas" class="mr-1 opacity-75" />
+                        {{$info['location']}}
+                    </li>
+                @endif
+                @if(!$info['blog']=="")
+                    <li>
+                        <icon icon="fa-globe" icon-style="fas" class="mr-1 opacity-75" />
+                        <a-styled :href="$info['blog']" :underlined="true">{{$info['blog']}}</a-styled>
+                    </li>
+                @endif
+                @if(isset($info['twitter_username']))
+                    <li>
+                        <icon icon="fa-twitter" icon-style="fab" class="mr-1 opacity-75" />
+                        <a-styled :href="'https://www.twitter.com/'.$info['twitter_username']" underlined>
+                            {{ '@'.$info['twitter_username'] }}
+                        </a-styled>
+                    </li>
+                @endif
+            </ul>
 
             @if(isset($info['bio']))
-            <div class="mt-2 mx-auto md:max-w-3xl py-2 text-2xl">  
+            <div class="py-2 mx-auto mt-2 text-2xl md:max-w-3xl">
                 <p>{{$info['bio']}}</p>
             </div>
             @endif
@@ -57,7 +56,7 @@
         </div>
     </hero>
 
-    <section class="container mx-auto flex flex-wrap">
+    <section class="container flex flex-wrap mx-auto">
         @foreach($packagist->only($packages)->sortByDesc('downloads.total') as $package)
             <package :package="$package" :github="$github" />
         @endforeach
