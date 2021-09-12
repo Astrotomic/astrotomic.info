82b78f1f24edfb47852a4dfbd80ca1ea7c962b8d
Tom Witkowski <tom.witkowski@frischepost.de>
2021-09-12 12:54:24 +0200

    update default config

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
-    'asset_url' => env('ASSET_URL', env('APP_URL', 'http://localhost')),
-    'mix_url' => env('MIX_URL', env('ASSET_URL', env('APP_URL', 'http://localhost'))),
+    'url' => env('APP_URL', 'http://localhost:8000'),
+    'asset_url' => env('ASSET_URL', env('APP_URL', 'http://localhost:8000')),
+    'mix_url' => env('MIX_URL', env('ASSET_URL', env('APP_URL', 'http://localhost:8000'))),
 
     /*
     |--------------------------------------------------------------------------
