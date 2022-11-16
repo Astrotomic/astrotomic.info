dd5f30664c832c46aaf803055c699156ef09c3bf
Tom Witkowski <dev.gummibeer@gmail.com>
2022-11-16 16:08:21 +0100

    add favicon to head

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,6 +8,8 @@
     <meta name="description" content="Astrotomic is an open source developer collective and wants to provide helpful, solid and easy to use open source packages.
 Most of them will be for Laravel - but sometimes also plain PHP." />
 
+    <link rel="shortcut icon" type="image/x-icon" href="{{ \Illuminate\Support\Facades\Vite::asset('resources/img/favicon.ico') }}" />
+
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
 </head>
